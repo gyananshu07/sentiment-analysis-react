b9deee35fab489cd8a6b6e2646a191199de3a0f7
gyananshu07 <2019041054@mmmut.ac.in>
2021-08-07 22:17:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Sentiment from "sentiment";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sentiment = new Sentiment();
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sentimentScore: 0,
+      generalSentiment: "Neutral",
+    };
+    this.findSentiment = this.findSentiment.bind(this);
+  }
+
+  findSentiment = (e) => {
+    const result = sentiment.analyze(e.target.value);
+    this.setState({
+      sentimentScore: result.score,
+    });
+    if (result.score < 0) {
+      this.setState({
+        generalSentiment: "Negative",
+      });
+    }
+    if (result.score > 0) {
+      this.setState({
+        generalSentiment: "Positive",
+      });
+    }
+    if (result.score === 0) {
+      this.setState({
+        generalSentiment: "Neutral",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Text Sentiment Analysis</h1>
+        <p>Enter text to get the real-time sentiment analysis:</p>
+        <textarea onChange={this.findSentiment} className="input"></textarea>
+        <div className="score-part">
+          <p className="score-p">
+            <span className="left">Sentiment Score: </span>
+            <span className="score">{this.state.sentimentScore}</span>
+          </p>
+          <p className="score-p">
+            <span className="left">General Sentiment: </span>
+            <span className="score">{this.state.generalSentiment}</span>
+          </p>
+        </div>
+        <div className="footer">
+          <h4>Developed by Gyananshu Kashyap</h4>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
